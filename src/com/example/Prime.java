c12b7be7df48e39ce4f239d421a54426f6ca942f
stenman <roger.stenman@gmail.com>
2016-02-18 21:59:58 +0100

    Added bracers

--- src/com/example/Prime.java
+++ src/com/example/Prime.java
@@ -30,8 +30,9 @@ public class Prime {
 		
 		long limit = (long) Math.sqrt(n) + 1;
 		for (long i = 6L; i <= limit; i += 6) {
-			if (n % (i - 1) == 0 || n % (i + 1) == 0)
+			if (n % (i - 1) == 0 || n % (i + 1) == 0) {
 				return false;
+			}
 		}
 		return true;
 	}
